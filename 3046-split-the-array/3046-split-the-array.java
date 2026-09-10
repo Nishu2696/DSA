f17544a398fcb0b77f367dd7fe8a639f736436ce
Nishaanth Vikram <nishaanth2696@gmail.com>
2026-09-10 14:25:44 +0530

    Time: 3 ms (25.38%), Space: 44.5 MB (60.98%) - LeetHub

---
+++ 3046-split-the-array/3046-split-the-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPossibleToSplit(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int key: map.keySet()) {
+            if (map.get(key) > 2) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
