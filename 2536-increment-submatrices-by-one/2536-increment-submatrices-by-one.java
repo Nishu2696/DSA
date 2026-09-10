979132bcd2aaff7450c52e13ee3d229c2ff005da
Nishaanth Vikram <nishaanth2696@gmail.com>
2026-09-10 14:02:17 +0530

    Time: 17 ms (49.55%), Space: 92.4 MB (12.54%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
@@ -1,28 +1,36 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        int[][] mat = new int[n][n];
-
-        for (int[] row: mat) {
-            Arrays.fill(row, 0);
-        }
+        int[][] diff = new int[n + 1][n + 1];
 
         for (int[] query: queries) {
-            int i = query[0];
-            int j = query[1];
+            int r1 = query[0];
+            int c1 = query[1];
+            int r2 = query[2];
+            int c2 = query[3];
 
-            int x = query[2];
-            int y = query[3];
+            diff[r1][c1] = diff[r1][c1] + 1;
+            diff[r1][c2 + 1] = diff[r1][c2 + 1] - 1;
+            diff[r2 + 1][c1] = diff[r2 + 1][c1] - 1;
+            diff[r2 + 1][c2 + 1] = diff[r2 + 1][c2 + 1] + 1;
+        }
 
-            int minI = Math.min(i, x);
-            int maxI = Math.max(i, x);
+        int[][] mat = new int[n][n];
 
-            int minJ = Math.min(j, y);
-            int maxJ = Math.max(j, y);
-
-            for (int a = minI; a <= maxI; a++) {
-                for (int b = minJ; b <= maxJ; b++) {
-                    mat[a][b] = mat[a][b] + 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0) {
+                    diff[i][j] = diff[i - 1][j] + diff[i][j];
                 }
+
+                if (j > 0) {
+                    diff[i][j] = diff[i][j - 1] + diff[i][j];
+                }
+
+                if (i > 0 && j > 0) {
+                    diff[i][j] = diff[i][j] - diff[i - 1][j - 1];
+                }
+
+                mat[i][j] = diff[i][j];
             }
         }
 
