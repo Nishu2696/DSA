d177fda2db4b16b3b882b4175a87dfcc88d378ef
Nishaanth Vikram <nishaanth2696@gmail.com>
2026-09-11 14:39:12 +0530

    Time: 11 ms (32.35%), Space: 155.8 MB (7.94%) - LeetHub

---
+++ 2055-plates-between-candles/2055-plates-between-candles.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] platesBetweenCandles(String s, int[][] queries) {
+        int n = s.length();
+        List<Integer> presum = new ArrayList<>();
+        int[] closestLeft = new int[n], closestRight = new int[n];
+        int sum = 0, index = -1;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '*') sum++;
+            else {
+                presum.add(sum);
+                index++;
+            }
+            closestLeft[i] = index;
+        }
+
+        index = presum.size();;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '|') index--;
+            closestRight[i] = index;
+        }
+
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int start = queries[i][0], end = queries[i][1];
+            int left = closestRight[start];
+            int right = closestLeft[end];
+            if (left < right) res[i] = presum.get(right) - presum.get(left);
+        }
+        return res;
+    }
+}
