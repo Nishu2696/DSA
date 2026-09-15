257fc4557c478a3cac883a86b9d0eb31dc44c55d
Nishaanth Vikram <nishaanth2696@gmail.com>
2026-09-15 12:12:33 +0530

    Time: 0 ms (100%), Space: 43.5 MB (41.87%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,15 +1,25 @@
 class Solution {
     public int hIndex(int[] citations) {
-        // [0, 1, 3, 5, 6]
-        Arrays.sort(citations);
-        int result = Integer.MIN_VALUE;
-        for (int i = 0; i < citations.length; i++) {
-            int value = citations[i];
-            int count = citations.length - i;
+        int n = citations.length;
+        int[] count = new int[n + 1];
 
-            result = Math.max(result, Math.min(value, count));
+        for (int num: citations) {
+            if (num >= n) {
+                count[n]++;
+            } else {
+                count[num]++;
+            }
         }
 
-        return result == Integer.MIN_VALUE ? 0 : result;
+        int result = 0;
+
+        for (int h = n; h >= 0; h--) {
+            result = result + count[h];
+            if (result >= h) {
+                return h;
+            } 
+        }
+
+        return 0;
     }
 }
